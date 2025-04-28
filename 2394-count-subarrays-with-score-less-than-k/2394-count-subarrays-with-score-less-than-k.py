9404c22ec5b3ffd6ecbf75c26852bd4e36097e6b
S SALIH AZEEM <s.salihazeem@gmail.com>
2025-04-28 17:26:11 +0530

    Update 2394-count-subarrays-with-score-less-than-k.py

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
@@ -11,3 +11,22 @@ class Solution(object):
                 left += 1
             count += (right - left + 1)
         return count
+
+
+
+
+
+
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            sum_ = 0
+            for j in range(i, n):
+                sum_ += nums[j]
+                length = j - i + 1
+                score = sum_ * length
+                if score < k:
+                    count += 1
+        return count
