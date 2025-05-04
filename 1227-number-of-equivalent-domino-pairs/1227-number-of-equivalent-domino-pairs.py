6323da1590c766d73daebc6c46db8bd036d44b59
S SALIH AZEEM <s.salihazeem@gmail.com>
2025-05-04 18:21:35 +0530

    Update 1227-number-of-equivalent-domino-pairs.py

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
@@ -7,3 +7,16 @@ class Solution(object):
             count += mpp[key]
             mpp[key] += 1
         return count
+
+
+
+class Solution(object):
+    def numEquivDominoPairs(self, dominoes):
+        count = 0
+        for i in range(len(dominoes)):
+            for j in range(i + 1, len(dominoes)):
+                a, b = dominoes[i]
+                c, d = dominoes[j]
+                if (a == c and b == d) or (a == d and b == c):
+                    count += 1
+        return count
