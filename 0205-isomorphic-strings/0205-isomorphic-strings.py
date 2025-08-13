25ec6fbe05ff2db6b2f31d48fc0d46190951df3a
S SALIH AZEEM <s.salihazeem@gmail.com>
2025-08-13 19:46:47 +0530

    Time: 10 ms (55.89%), Space: 13.1 MB (87.45%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -5,5 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return [s.index(c) for c in s] == [t.index(c) for c in t]
-        
+        map1={}
+        map2={}
+        p=[]
+        q=[]
+        c=1
+        for i in s:
+            if i not in map1:
+                map1[i] = str(c)
+                c+=1
+            p.append(map1[i])
+        c=1
+        for i in t:
+            if i not in map2:
+                map2[i] = str(c)
+                c+=1
+            q.append(map2[i])
+        return p==q
