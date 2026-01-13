39f19109bc1a1cbd5e57c42df96c691db7c37959
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-01-13 12:55:24 +0530

    Time: 0 ms (100%), Space: 43 MB (74.67%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim(); 
+        int n = s.length();
+        int len = 0 ;
+        if(n == 0) return n;
+        
+        for(int i = n-1 ; i >= 0 ; i--){
+            if( s.charAt(i) != ' '){
+                len++;
+            }else{
+                break;
+            }
+        }
+        return len;
+    }
+}
