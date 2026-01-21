66f4b00f87a8e87399690e4c498064e7d3e3146e
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-01-21 06:55:36 +0530

    Time: 11 ms (3.15%), Space: 43.8 MB (6.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l = 0 ; 
+        int m = 0 ;
+        int h = nums.length - 1 ;
+        while( m <= h ){
+            if(nums[m] == 0){
+                int t = nums[l];
+                nums[l] = nums[m];
+                nums[m] = t;
+                l++;
+                m++;
+            }else if(nums[m] == 1){
+                m++;
+            }else{
+                int t = nums[h];
+                nums[h] = nums[m];
+                nums[m] = t ;
+                h--;
+            }
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            if(i==0)System.out.print("[");
+            System.out.print(nums[i]);
+            if(i<nums.length - 1)System.out.print(",");
+            if(i==nums.length - 1)System.out.print("]");
+        }
+        
+    }
+}
