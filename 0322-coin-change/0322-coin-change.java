8274f652d8583d27c8fa758bebcd866878664295
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-01-26 18:40:18 +0530

    Time: 14 ms (88.55%), Space: 46.2 MB (88.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -3,15 +3,18 @@ class Solution {
 
         int[] dp = new int[amount+1];
         Arrays.fill(dp,amount+1);
-        dp[0] = 0;
-        for(int i = 1 ; i <= amount ; i++){
-            for(int c : coins ){
-                if( i - c >= 0 ){
-                    dp[i] = Math.min( dp[i] , dp[i-c] + 1 );
+        dp[0] = 0 ;
+        for(int i = 1 ; i < dp.length ; i ++){
+            for(int c : coins){
+                if(i-c>=0){
+                    dp[i] = Math.min(dp[i] , 1+ dp[i-c]);
                 }
             }
         }
-        int res = dp[amount] > amount ? -1 : dp[amount];
-        return res;
+        int n = dp.length;
+        if(dp[amount] <= amount){
+            return dp[n-1];
+        }
+        return -1;
     }
 }
