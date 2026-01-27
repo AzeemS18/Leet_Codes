cb8b8327cac029e644cbac599e360843a3328717
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-01-27 21:35:40 +0530

    Time: 3 ms (87.12%), Space: 48.4 MB (18.79%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -2,14 +2,23 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int r = matrix.length;
         int c = matrix[0].length;
-        for(int i = 0 ; i < r ; i++ ){
-            for(int j = 0 ; j < c ; j++ ){
-            int x = matrix[i][j];
-            if(x==target){
+        int n = c - 1 ;
+        int i = 0 ;
+        
+        while( i < r && n >= 0){
+            int current = matrix[i][n];
+            if( current == target ){
                 return true;
             }
+            if(current > target){
+                n-- ;
             }
+            if(current < target){
+                i++ ;
+            }
+
         }
         return false;
+        
     }
 }
