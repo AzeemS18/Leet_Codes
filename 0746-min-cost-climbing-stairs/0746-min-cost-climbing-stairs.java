98d66c8662633fb450b9a9c15e8017067992a8ca
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-01-29 15:10:42 +0530

    Time: 1 ms (38.7%), Space: 45.2 MB (18.76%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,12 +1,20 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for(int i = 2 ; i < cost.length ; i++){
-            dp[i] = cost[i] + Math.min(dp[i-1] , dp[i-2]);
-        }
-        return Math.min(dp[cost.length - 1] , dp[cost.length - 2]);
+        int l = cost.length - 1;
+        int[] dp = new int[l+1];
+        Arrays.fill(dp,-1);
+        int n1 = minc( cost , l , dp);
+        int n2 = minc( cost , l - 1, dp);
+        return Math.min(n1,n2);
     }
+    public int minc(int[] a , int s ,int[] dp){
+        if( s == 0 )return a[0];
+        if( s == 1) return a[1];
+        if(dp[s] != -1){
+            return dp[s];
+        }
+        return dp[s] = Math.min (minc(a,s-1,dp),minc(a,s-2,dp)) + a[s];
+    }
+
     
 }
