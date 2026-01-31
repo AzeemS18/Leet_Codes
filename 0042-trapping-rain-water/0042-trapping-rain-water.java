3495b8ad8b61611dbcc9d16a198deef74a449887
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-01-31 16:19:30 +0530

    Time: 6 ms (6.09%), Space: 48.7 MB (28.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        int max = 0 ;
-        if(height.length < 0 ) return max;
-        int n = height.length;
-        int[] lm = new int[n+1];
-        int[] rm = new int[n+1];
-        lm[0] = height[0];
-        rm[n-1] = height[n-1];
+        Stack<Integer> st = new Stack<>();
+        int w = 0;
+        for(int r = 0 ; r < height.length ; r++){
+            while( !st.isEmpty() && height[r] > height[st.peek()] ){
+                int b = st.pop();
+                if(st.isEmpty()) break;
+                int l = st.peek();
+                int wi = r - l - 1;
+                int minw = Math.min(height[r],height[l]) - height[b];
+                w += wi * minw;
 
-        for(int i = 1 ; i < n ; i++){
-            lm[i] = Math.max(height[i],lm[i-1]);
+            }
+            st.push(r);
         }
-        for(int i = n-2 ; i >= 0 ; i--){
-            rm[i] = Math.max(height[i],rm[i+1]);
-        }
-        for(int i = 0 ; i < n ; i++){
-            max += Math.min(lm[i],rm[i]) - height[i];
-        }
-
-        return max;
+        return w; 
+        
     }
 }
