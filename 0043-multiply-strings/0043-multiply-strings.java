95cdf37c6b55342acefe8b7a4220bd455b0a8817
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-02-09 21:56:52 +0530

    Time: 3 ms (79.74%), Space: 43.6 MB (80.53%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,10 +1,24 @@
-import java.math.BigInteger;
 class Solution {
-    public String multiply(String num1, String num2) {
-        BigInteger x = new BigInteger(num1);
-        BigInteger y = new BigInteger(num2);
-        BigInteger z = x.multiply(y);
-        return z.toString();
+    public String multiply(String nums1, String nums2) {
+        int n = nums1.length();
+        int m = nums2.length();
+        int[] res = new int[n+m];
+        for(int i = n-1 ; i >=0 ; i--){
+            int d1 = nums1.charAt(i) - '0' ;
+            for(int j = m-1 ; j >= 0 ; j--){
+                int d2 = nums2.charAt(j) - '0' ;
+                int s = d1 * d2 + res[i+j+1];
+                res[i+j+1] = s%10;
+                res[i+j] += s / 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i : res){
+            if(!(sb.length() == 0 && i == 0) ){
+                sb.append(i);
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
         
     }
 }
