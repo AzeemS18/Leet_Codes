482046e639d5ef1e7ddc38d8a884b178e3e2776c
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-02-11 19:33:05 +0530

    Time: 0 ms (100%), Space: 43.8 MB (69.52%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] a) {
+        int n = a.length;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
+                int t = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = t;
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            int l = 0 ;
+            int r = n - 1 ;
+            while(l < r){
+                int t = a[i][l];
+                a[i][l] = a[i][r];
+                a[i][r] = t;
+                l++;
+                r--;
+            }
+        }
+        
+    }
+}
