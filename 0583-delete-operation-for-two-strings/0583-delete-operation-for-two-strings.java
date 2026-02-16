4346f200d4825f0c085aad868092d55e0bbba537
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-02-16 22:05:36 +0530

    Time: 7 ms (71.95%), Space: 46.7 MB (13.68%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -3,22 +3,18 @@ class Solution {
         int n = w1.length();
         int m = w2.length();
         int[][] dp = new int[n+1][m+1];
-        for(int i = 0 ; i <= n ; i++){
-            dp[i][0] = i;
-        }
-        for(int i = 0 ; i <= m ; i++){
-            dp[0][i] = i;
-        }
+
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
                 if(w1.charAt(i-1) == w2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j] , dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
                 }
             }
         }
-        return dp[n][m];
+        int res =  n + m -  (2 * dp[n][m]);
+        return res;
 
         
     }
