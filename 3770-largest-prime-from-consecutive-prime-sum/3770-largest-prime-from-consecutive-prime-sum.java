dc8417f188fdb96c727655defdb646de38096aaf
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-06-16 18:40:19 +0530

    Time: 1031 ms (6.36%), Space: 273.7 MB (5.2%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.java
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int largestPrime(int n) {
+        if(n < 2)return 0;
         if(n==2)return 2;
         boolean[] a = new boolean[n+1];
         Arrays.fill(a,true);
@@ -13,16 +14,17 @@ class Solution {
             }
         }
         int sum = 0 ;
-        int res = 0;
+        int res = 0 ;
+        HashSet<Integer> m = new HashSet<>();
         for(int i = 2 ; i < n ; i++){
             if(a[i]){
                 sum += i;
-                if(sum > n){
-                    break;    
-                }
-                if(a[sum]){
-                    res = sum;
-                }
+                m.add(sum);
+            }
+        }
+        for(int i = n ; i > 0 ; i--){
+            if(m.contains(i) && a[i]){
+                return i;
             }
         }
         return res;
