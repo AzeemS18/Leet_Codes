8be583b0b473526b6cb48a6a3f0573982cdc2c70
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-06-18 07:01:05 +0530

    Time: 92 ms (5.32%), Space: 48.5 MB (73.25%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,15 +1,31 @@
+import java.util.*;
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        if( (arr.length <= start) || start < 0 || arr[start] < 0){
-            return false;
-        }
-        if(arr[start] == 0){
-            return true;
-        }
-        int j = arr[start];
-        arr[start] = -arr[start];
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(start);
+        while(!q.isEmpty()){
+            
+            int c = q.poll();
+            System.out.print(c);
+            if( arr[c] == 0 ){
+                return true;
+            }
+            if( arr[c] < 0 ){
+                continue;
+            }
+            int f = c + arr[c];
+            int b = c - arr[c];
 
-        return canReach(arr,start+j) || canReach(arr,start-j);
-        
+            arr[c] = -arr[c];
+
+            if( f < arr.length && arr[f] >= 0){
+                q.add(f);
+            }
+            if( b >= 0 && arr[b] >= 0){
+                q.add(b);
+            }  
+        }
+        return false;
+
     }
 }
