acdec6662cba12e7de2d6e772a5d7dce352fa4b2
S SALIH AZEEM <s.salihazeem@gmail.com>
2026-07-25 22:02:30 +0530

    Time: 6 ms (87.77%), Space: 45.1 MB (79.29%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> s = new Stack<>();
+
+        for(String i : tokens){
+            if(i.equals("+")){
+                s.push(s.pop()+s.pop());
+            }else if(i.equals("-")){
+                int a = s.pop();
+                int b = s.pop();
+                s.push(b-a);
+            }else if(i.equals("*")){
+                int a = s.pop();
+                int b = s.pop();
+                s.push(a*b);
+            }else if(i.equals("/")){
+                int a = s.pop();
+                int b = s.pop();
+                s.push(b/a);
+            }else{
+                s.push(Integer.valueOf(i));
+            }
+
+        }
+        return s.pop();
+        
+    }
+}
